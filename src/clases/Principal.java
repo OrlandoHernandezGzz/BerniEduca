1f4bd522f7d678ddf6d0392a608061da6f242c3a
PROPIETARIO <PROPIETARIO@Orlando-PC>
2020-10-10 19:15:39 -0500

    hola programadores

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -4,5 +4,6 @@ package clases;
 public class Principal {
     public static void main(String args[]){
         System.out.println("Hola mundo");
+        System.out.println("Listos para programar?");
     }
 }
