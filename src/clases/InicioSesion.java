707cc696b8b5441419fe57880db0e10198fe2bf0
PROPIETARIO <PROPIETARIO@Orlando-PC>
2020-10-12 23:42:43 -0500

    Aporte Orlando Se agregaron los diseños de los menus de login y menu principal

--- src/clases/InicioSesion.java
+++ src/clases/InicioSesion.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class InicioSesion {
